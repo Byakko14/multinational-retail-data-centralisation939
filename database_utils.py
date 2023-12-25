d4b19e2fe44d8333d21c9179ac38470001d615cf
Thirarit Chantschool <Rit@192.168.1.101>
2023-12-25 18:01:47 +0000

    updates

--- database_utils.py
+++ database_utils.py
@@ -22,9 +22,9 @@ class DatabaseConnector:
 
     def list_db_tables(self, engine):
         # List all tables in the connected database
-        inspector = inspect(engine)
+        inspector = inspect(self._init_db_engine())
         return inspector.get_table_names()
 
     def upload_to_db(self, engine, table_name, data):
         # Upload data to the specified table in the database
-        data.to_sql(table_name, engine, if_exists='replace', index=False)
+        data.to_sql(table_name, self._init_db_engine(), if_exists='replace', index=False)
